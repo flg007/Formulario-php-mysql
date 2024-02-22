5e7fb3cc9079fd9d641f3a3b607e6e648404a24b
flg007 <florentinolutinogonzales@gmail.com>
2024-02-21 22:45:06 -0400

    realize unos cambios basicos en index.php agregue un form y una imagen y cambie de titulo

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro</title>
+</head>
+<body>
+
+    <form methnod="post" >
+        <h2>Hola</h2>
+        <p>Inicia tu registro</p>
+
+        <div class="input-wrapper">
+            <input type="text" name="name" placeholder="Nombre" >
+            <img class="input-icon" src="images/name.svg" alt="">
+
+        </div>
+
+
+
+
+    </form>
+    
+</body>
+</html>
