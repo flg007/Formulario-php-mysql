4ed54479a53eb084d2d4ba89e40e4d9d85a7be1c
flg007 <florentinolutinogonzales@gmail.com>
2024-02-22 09:56:40 -0400

    tuve problemas de sintaxis y tuve que revisar todo el codigo, pense que era css y habia sido el error en index.php donde no cerre el div </div>

--- index.php
+++ index.php
@@ -2,32 +2,32 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-    <form methnod="post" >
+    <form method="post">
+
         <h2>Hola</h2>
         <p>Inicia tu registro</p>
 
         <div class="input-wrapper">
             <input type="text" name="name" placeholder="Nombre" >
             <img class="input-icon" src="images/name.svg" alt="">
-
         </div>
 
         <div class="input-wrapper">
             <input type="email" name="email" placeholder="Email" >
             <img class="input-icon" src="images/email.svg" alt="">
-
         </div>
 
         <div class="input-wrapper">
-            <input type="tet" name="direction" placeholder="Direccion" >
+            <input type="text" name="direction" placeholder="Direccion" >
             <img class="input-icon" src="images/direction.svg" alt="">
-
+        </div>
         
         <div class="input-wrapper">
             <input type="tel" name="phone" placeholder="Telefono" >
