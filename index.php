0aa24cb94a88da1753b4869859d6f736775ed571
tnt007-FLG <lutinogonzales@gmail.com>
2024-02-23 11:34:51 -0400

    solo queria guardar el archivo .txt que viene siendo la solucion a los errores que tuve al reallizar este proyecto

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro</title>
+</head>
+<body>
+    
+<form method="post">
+
+<h2> Hola</h2>
+<p>Inicia tu registro</p>
+
+<div class="input-wrapper">
+    <input type="text" name="name" placeholder="Nombre">
+    <img class="input-icon"  src="images/name.svg" alt="">
+</div>
+</form>
+
+</body>
+</html>
